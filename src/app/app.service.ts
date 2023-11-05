bcd76c22257ec79dac9b32c5e1b2006679defc41
Santiago Forero A <ds.foreroa@uniandes.edu.co>
2023-11-04 23:59:21 -0500

    feat: add AppService and Coffee model

---
+++ src/app/app.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Coffee } from './coffee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+  constructor(private http: HttpClient) {}
+
+  getStock(): Observable<Coffee[]> {
+    let url =
+      'https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json';
+    return this.http.get<Coffee[]>(url);
+  }
+}
