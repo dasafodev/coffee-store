a1020a386ca457420769ccc2e5589dfc401a0f09
Santiago Forero A <ds.foreroa@uniandes.edu.co>
2023-11-05 00:54:51 -0500

    feat(coffee-list): add calculation for coffee types

--- src/app/coffee-list/coffee-list.component.ts
+++ src/app/coffee-list/coffee-list.component.ts
@@ -9,12 +9,24 @@ import { CoffeeListService } from './coffee-list.service';
 })
 export class CoffeeListComponent {
   coffeeList: Coffee[] = [];
+  numCafeDeOrigen: number = 0;
+  numCafeBlend: number = 0;
 
   constructor(private coffeeListService: CoffeeListService) {}
 
   ngOnInit() {
     this.coffeeListService.getStock().subscribe((data: Coffee[]) => {
       this.coffeeList = data;
+      this.calculateCoffeeTypes();
     });
+  }
+
+  calculateCoffeeTypes() {
+    this.numCafeDeOrigen = this.coffeeList.filter(
+      (coffee) => coffee.tipo === 'Café de Origen'
+    ).length;
+    this.numCafeBlend = this.coffeeList.filter(
+      (coffee) => coffee.tipo === 'Blend'
+    ).length;
   }
 }
